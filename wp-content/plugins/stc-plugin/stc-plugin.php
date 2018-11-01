3afcc00d7f5abe10c547fc136c6f83c185e4430d
tobikars <tobikars@gmail.com>
2018-11-01 14:38:58 +0800

    added basic file, license and logo

---
+++ wp-content/plugins/stc-plugin/stc-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/** 
+ * @package STCPlugin
+ */
+
+ /*
+ Plugin Name: ScanTrust Consumer Plugin
+ Plugin URI: http://localhost:8000
+ Description: This is the Official ScanTrust Plugin for Wordpress. It can be used to display code, scan, product and scm data.
+ Version: 0.0.1
+ Author: Tobias Kars
+ Author URI: https://www.linkedin.com/in/tobikars/
+ License: GPLv2 or later
+ Text Domain: scantrust-plugin
+ */
