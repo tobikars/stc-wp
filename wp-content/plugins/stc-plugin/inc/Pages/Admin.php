f4d3769b76a857b9a6ccedc8cfa73b5fe4aed1d0
tobikars <tobikars@gmail.com>
2018-11-08 07:49:42 +0800

    added submenu and readme

--- wp-content/plugins/stc-plugin/inc/Pages/Admin.php
+++ wp-content/plugins/stc-plugin/inc/Pages/Admin.php
@@ -6,20 +6,83 @@
 namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
+use \Inc\Api\SettingsApi;
 
 class Admin extends BaseController {
 
+    public $settings;
+    public $pages = array();
+    public $subpages = array();
+
+
+    public function __construct() 
+    {
+        parent::__construct();
+
+        $this->settings = new SettingsApi();
+
+        $this->pages = [
+            [
+                'page_title' => 'ScanTrust Plugin', 
+                'menu_title' => 'ScanTrust', 
+                'capability' => 'manage_options',
+                'menu_slug' => 'scantrust_plugin',
+                'callback' => function() { echo '<h1>STC Plugin General Settings</h1>'; },
+                'icon_url' => $this->plugin_icon, 
+                'position' => 110
+            ]
+        ];
+
+        $this->subpages = [
+            [
+                'parent_slug' => 'scantrust_plugin', 
+                'page_title' => 'Custom Post Types', 
+                'menu_title' => 'CPT', 
+                'capability' => 'manage_options',
+                'menu_slug' => 'scantrust_plugin_cpt',
+                'callback' => function() { echo '<h1>Custom Post Types For ScanTrust Use (tutorial)</h1>'; }
+            ],
+            [
+                'parent_slug' => 'scantrust_plugin', 
+                'page_title' => 'Endpoint & API Settings', 
+                'menu_title' => 'Endpoint', 
+                'capability' => 'manage_options',
+                'menu_slug' => 'scantrust_plugin_endpoint',
+                'callback' => function() { echo '<h1>Endpoint & API Settings</h1>'; }
+            ], 
+            [
+                'parent_slug' => 'scantrust_plugin', 
+                'page_title' => 'Campaigns', 
+                'menu_title' => 'Campaign Data', 
+                'capability' => 'manage_options',
+                'menu_slug' => 'scantrust_plugin_campaigns',
+                'callback' => function() { echo '<h1>Campaign Settings</h1>'; }
+            ],
+            [
+                'parent_slug' => 'scantrust_plugin', 
+                'page_title' => 'Product Settings', 
+                'menu_title' => 'Product Data', 
+                'capability' => 'manage_options',
+                'menu_slug' => 'scantrust_plugin_products',
+                'callback' => function() { echo '<h1>Product Settings</h1>'; }
+            ],
+            [
+                'parent_slug' => 'scantrust_plugin', 
+                'page_title' => 'Scan Data Collection', 
+                'menu_title' => 'Scan Data', 
+                'capability' => 'manage_options',
+                'menu_slug' => 'scantrust_plugin_scandata',
+                'callback' => function() { echo '<h1>Scan Data Collection</h1>'; }
+            ]
+        ];
+
+
+
+
+    }
+
     public function register() {
-        add_action('admin_menu', array( $this, 'add_admin_pages'));
-    }
-
-    function add_admin_pages() {
-        add_menu_page(('ScanTrust Plugin'), 'ScanTrust', 'manage_options', 'scantrust_plugin', array( $this, 'admin_index'), $this->plugin_icon, 110);
-    }
-
-    function admin_index() {
-        // require template
-        require_once $this->plugin_path . 'templates/admin.php';
+        $this->settings->addPages( $this->pages )->withSubPage('Dashboard')->addSubPages( $this->subpages )->register();
     }
 
  }
---
+++ wp-content/plugins/stc-plugin/readme.md
@@ -0,0 +1,28 @@
+# ScanTrust Consumer Plugin Features
+
+## ScanTrust Plugin Features:
+
+* Custom Post Types to include STC functionality
+* Admin Area to manage settings
+  - Custom Post Types
+  - Endpoint management & API keys
+  - Campaign
+  - Product
+  - Scan Data Collection
+* Demo-pages with shortcodes
+
+## Tutorial Features: 
+
+List of demo-features for a plugin, from https://www.youtube.com/watch?v=TYhNcRQShig&index=16&list=PLriKzYyLb28kR_CPMz8uierDWC2y3znI2 
+
+* Modular Admin Area
+* CPT Manager
+* Custom Taxonomy Manager
+* Widget to Upload Apply Media in Sidebars
+* Post and Pages Gallery integration
+
+* Testimonial Section: Comment in the front end, Admins can approve comments and select which comments to display. 
+* Custom template section
+* Ajax-based Login/Register system
+* Membership-protected area
+* Add Chat-system based on a JS framework
